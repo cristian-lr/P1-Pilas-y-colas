f92b7e2afdcfe532ed91a8d93f3a1ea28d4da190
cristian-lr <126866269+cristian-lr@users.noreply.github.com>
2023-05-21 23:12:21 +0100

    feat(Main): Menu principal

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,53 @@
+import net.salesianos.lacuesta.cajero.Cajero;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Cajero cajero = new Cajero();
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
+        int opcion;
+        while (!salir) {
+            System.out.println("Menú:");
+            System.out.println("1.Abrir caja");
+            System.out.println("2.Añadir un nuevo cliente a la cola");
+            System.out.println("3.Atender un cliente");
+            System.out.println("4.Ver clientes pendientes");
+            System.out.println("5.Cerrar supermercado");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcion) {
+                case 1:
+                    cajero.abrirCaja();
+                    break;
+
+                case 2:
+                    cajero.aniadirCliente();
+                    break;
+
+                case 3:
+                    cajero.atenderCliente();
+                    break;
+
+                case 4:
+                    cajero.verClientesPendientes();
+                    break;
+
+                case 5:
+                    cajero.cerrarSupermercado();
+                    salir = true;
+                    System.out.println("Supermercado cerrado");
+
+                    break;
+
+                default:
+                    System.out.println("Número de opción erróneo");
+                    break;
+            }
+
+        }
 
     }
 }
